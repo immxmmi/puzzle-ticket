36e01a0bc402f3fb951d66d46b7f0a2e5428d220
immxmmi <immxmmi@gmx.at>
2025-07-30 13:05:05 +0200

    Add OpenTelemetry integration and improve error handling in Ruby test script

--- cicd-team-organisation#416/language/ruby/test.rb
+++ cicd-team-organisation#416/language/ruby/test.rb
@@ -1,8 +1,29 @@
 require 'sentry-ruby'
 
+require 'opentelemetry/sdk'
+require 'opentelemetry/exporter/otlp'
+
+OpenTelemetry::SDK.configure do |c|
+  c.service_name = 'ruby-glitchtip'
+  c.use_all
+end
+
+tracer = OpenTelemetry.tracer_provider.tracer('glitchtip-ruby-tracer')
+
+tracer.in_span('test-span') do |span|
+  puts "Tracing inside test-span"
+end
+
 Sentry.init do |config|
   config.dsn = 'https://69d0738116074e24a0f6a0727834737e@glitchtip-stg.puzzle.ch/10'
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+  config.environment = 'development'
+  config.debug = true
 end
 
-raise "Test error for GlitchTip Ruby"
+begin
+  raise "Test error for GlitchTip Ruby"
+rescue => e
+  Sentry.capture_exception(e)
+  raise
+end
